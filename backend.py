511302d03b5ef039d5b0bd9aee7ff6d067f1f30c
lalahaseeb816-cmd <lalahaseeb816@gmail.com>
2026-09-17 23:54:23 +0530

    Update backend.py

--- backend.py
+++ backend.py
@@ -18,6 +18,10 @@ def get_db():
     conn = sqlite3.connect("hs_clothing.db")
     conn.row_factory = sqlite3.Row
     return conn
+    
+    @app.route("/")
+def home():
+    return render_template("index.html")
 
 
 def create_database():
